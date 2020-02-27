7250968d65b25cb7fa9391d088f0d087c27fc5bf
flyingicedragon <1229760716@qqcom>
2020-02-27 11:29:16 +0800

    对程序进行简单封装，使其可以在命令行中直接调用

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
+# gff2gggene
+
+## 依赖情况
+
 本工具依赖于 gffutils 包。
 
-详细的使用情况请参考脚本内的注释。
-两个输出文件分别是包含所有 genes 的表格和包含所有 sub items 的表格。
+## 使用说明
+
+### 安装依赖
+
+使用 `conda` 或者 `pip` 进行安装。
+
+``` powershell
+conda install gffutils
+pip install gffutils
+```
+
+### gff 文件预处理
+
+对 gff 文件进行预处理，截取仅包含所需基因的 gff 内容。建议在 Linux 中使用 `sed` 命令完成。
+
+### 程序调用
+
+Windows 中利用 powershell 调用程序：
+
+``` powershell
+python .\gff2gggene.py example.gff
+python .\gff2gggene.py example.gff sub
+```
+
+Linux 中利用 console 调用程序：
+
+``` shell
+python ./gff2gggene.py example.gff
+python ./gff2gggene.py example.gff sub
+```
+
+根据安装 Python 的版本不同，可能需要将“python”替换为“python3”。
+
+不添加 `sub` 参数，表示将各基因的情况进行输出；添加 `sub` 参数，表示输出各基因子区域（例如：mRNA、CDS等，与 gff 文件内容有关）。
+
+### 结果输出
+
+界面显示“完成”表示程序运行成功。
--- gff2gggene.py
+++ gff2gggene.py
@@ -1,9 +1,20 @@
 import gffutils
+import sys
 
+filePath = ''
+sub = False
+if len(sys.argv) == 2:
+    filePath = str(sys.argv[1])
+elif len(sys.argv == 3):
+    filePath = str(sys.argv[1])
+    sub = True
+else:
+    print('请正确输入参数')
 # 导入 gff 文件，会生成数据库
-# gffData = gffutils.create_db('tem.gff', dbfn='tem.db')
+fileName = filePath.split('.')[0]
+gffData = gffutils.create_db(filePath, dbfn=fileName + '.db')
 # 导入数据库
-gffData = gffutils.FeatureDB('tem.db', keep_order=True)
+# gffData = gffutils.FeatureDB(filePath, keep_order=True)
 # 初始化列表
 gffGene = ['molecule,gene,start,end,strand,direction']
 gffSubGene = ['molecule,gene,start,end,strand,subgene,from,to']
@@ -20,16 +31,20 @@ for i in gffData.all_features(featuretype='gene'):
     # 遍历 gene 的所有子项，如mRNA等
     for j in gffData.children(i.id):
         # 组装 subgene 列表字符串并添加
-        gffSubGene.append('tem,' + i['Name'][0] + ',' + str(i.start) + ',' + str(i.end) + ',' + strand + ',' + j.featuretype + ',' + str(j.start) + ',' + str(j.end))
+        gffSubGene.append('tem,' + i['Name'][0] + ',' + str(i.start) + ',' + str(i.end) + ',' + strand + ',' + j.featuretype + ',' + str(j.start) + ',' +
+                          str(j.end))
 # 将列表字符串拼接
 gffGeneContent = '\n'.join(gffGene)
 gffGeneContent += '\n'
 gffSubGeneContent = '\n'.join(gffSubGene)
 gffSubGeneContent += '\n'
 # 导出
-temGene = open('temGene.csv', 'w')
-temGene.write(gffGeneContent)
-temSubGene = open('temSubGene.csv', 'w')
-temSubGene.write(gffSubGeneContent)
-temGene.close()
-temSubGene.close()
+if sub:
+    temSubGene = open(fileName + '_SubGene.csv', 'w')
+    temSubGene.write(gffSubGeneContent)
+    temSubGene.close()
+else:
+    temGene = open(fileName + '_Gene.csv', 'w')
+    temGene.write(gffGeneContent)
+    temGene.close()
+print('完成')
